d076cda8976b50c853b8f775c0ea7fc5e10a8873
Varun Vasudevan <the.flazer@gmail.com>
2017-08-11 15:42:03 +0800

    Included ngrok

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # load-randomizer
+
+Simple NodeJS server to randomly forward your traffic to two separate servers. This was built to test out how web application would behave when put behind a load balancer.
+
+By default this server forwards requests to `3000` and `3001`
+
+To install
+```
+npm install
+```
+
+To start the server with default ports:
+```
+npm start
+```
+
+To run with custom ports `3002` and `3003`
+```
+npm start 3002 3003
+```
+
+To run with custom ports `3002` and `3003` and use ngrok to make ports globally available
+```
+npm start 3002 3003 ngrok
+```
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const _ = require('lodash');
 const request = require('request');
 const express = require('express');
-
+const ngrok = require('ngrok');
 const app = express();
 
 const server = require('http').createServer(app);
+const listenPort = 4000;
 
-server.listen(4000, '0.0.0.0', () => {
+
+server.listen(listenPort, '0.0.0.0', () => {
+  if (_.includes(process.argv, 'ngrok')) {
+    ngrok.connect(listenPort, (err, url) => {
+      if (err) {
+        console.log('There was an error setting up your tunnel')
+      }
+      console.log(`Access available at ${url}`);
+    })
+  }
   console.log('Express server listening on 4000');
   app.use('/', (req, res) => {
-    var url = _.round(Math.random()) === 1 ? 'http://0.0.0.0:3000' : 'http://0.0.0.0:3001';
+    var url = _.round(Math.random()) === 1 ?
+      _.get(process, 'argv[2]', 'http://0.0.0.0:3000')
+      :
+      _.get(process, 'argv[3]', 'http://0.0.0.0:3001')
     console.log(`Request for '${req.path}' => ${url}`);
     req.pipe(request(url)).pipe(res);
   });
