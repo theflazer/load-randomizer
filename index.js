2d22be29fa99e62db4c7cb1a08cab433eeba48d6
Varun Vasudevan <the.flazer@gmail.com>
2017-08-11 15:55:07 +0800

    Fixed ngrok bug

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express();
 const server = require('http').createServer(app);
 const listenPort = 4000;
 
+const getPorts = () => {
+  return _.reduce(process.argv, (result, arg) => {
+    if (!_.isNaN(parseInt(arg, 10))) {
+      result.push(arg);
+    }
+    return result;
+  }, []);
+}
 
 server.listen(listenPort, '0.0.0.0', () => {
   if (_.includes(process.argv, 'ngrok')) {
@@ -14,15 +22,16 @@ server.listen(listenPort, '0.0.0.0', () => {
       if (err) {
         console.log('There was an error setting up your tunnel')
       }
-      console.log(`Access available at ${url}`);
+      console.log('Access available at\x1b[36m', `${url}`, '\x1b[0m');
     })
   }
   console.log('Express server listening on 4000');
   app.use('/', (req, res) => {
+    const ports = getPorts();
     var url = _.round(Math.random()) === 1 ?
-      _.get(process, 'argv[2]', 'http://0.0.0.0:3000')
+      `http://0.0.0.0:${_.get(ports, '[0]', 3000)}`
       :
-      _.get(process, 'argv[3]', 'http://0.0.0.0:3001')
+      `http://0.0.0.0:${_.get(ports, '[1]', 3001)}`
     console.log(`Request for '${req.path}' => ${url}`);
     req.pipe(request(url)).pipe(res);
   });
